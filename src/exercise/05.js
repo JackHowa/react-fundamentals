690a017dc5828842523f4b2e86c3c7285e25a920
Jack Howard <jackhowaa@gmail.com>
2020-11-08 17:51:03 -0600

    Spread the classname and style object

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,37 +4,29 @@
 import React from 'react'
 import '../box-styles.css'
 
-const smallBox = (
-  <div
-    className="box box--small"
-    style={{fontStyle: 'italic', backgroundColor: 'lightblue'}}
-  >
-    small lightblue box
+const Box = ({className, style, children}) => {
+  return (
+    <div
+      className={`box ${className}`}
+      style={{fontStyle: 'italic', ...style}}
+    >
+    {children}
   </div>
-)
-const mediumBox = (
-  <div
-    className="box box--medium"
-    style={{fontStyle: 'italic', backgroundColor: 'pink'}}
-  >
-    medium pink box
-  </div>
-)
-const largeBox = (
-  <div
-    className="box box--large"
-    style={{fontStyle: 'italic', backgroundColor: 'orange'}}
-  >
-    large orange box
-  </div>
-)
+  )
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box className={"box--small"} style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box className={"box--medium"} style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box className={"box--large"} style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
     </div>
   )
 }
