ba1843ba1658e7a4b0db16c5b97b513644270503
Jack Howard <jackhowaa@gmail.com>
2020-11-08 18:47:59 -0600

    Basic error form handling, kinda wonky error message

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,22 +1,37 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const [isError, setError] = useState('Type something')
   const usernameRef = useRef('')
   const handleSubmit = (event) => {
     event.preventDefault();
     onSubmitUsername(usernameRef.current.value)
   }
 
+  const isLowerCase = (string) => string === string.toLowerCase();
+
+  const handleChange = (event) => {
+    const newUsernameValue = event.target.value;
+    if (!isLowerCase(newUsernameValue)) {
+      setError('Username must be lower case')
+    } else if (!newUsernameValue) {
+      setError('Type something')
+    } else {
+      setError(false)
+    }
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username:</label>
-        <input ref={usernameRef} id="username" type="text" />
+        <input onChange={handleChange} ref={usernameRef} id="username" type="text" />
+        {isError && <p role="alert">{isError}</p>}
       </div>
-      <button type="submit">Submit</button>
+      <button disabled={isError} type="submit">Submit</button>
     </form>
   )
 }
